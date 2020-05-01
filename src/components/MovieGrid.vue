52a6ca583f8cdba62f6472a15d3e22d46cc1339c
Mohamed Nizamuddin <mohamed.nizamuddin10a@gmail.com>
2020-05-01 10:12:56 +0200

    add loading animation for movieGrid

--- src/components/MovieGrid.vue
+++ src/components/MovieGrid.vue
@@ -2,8 +2,14 @@
     <v-layout class="ma-8" justify-start row wrap>
         <v-flex xs6 sm4 md3 lg2 v-for="(movie, i) in movies" :key="i">
             <router-link class="ml-2 pb-8 titre" :title="(movie.title || movie.original_title)" :to="{ name: 'Movie', params: { id: movie.id } }">
-                <v-card class="mb-2" raised width="187" color="grey lighten-2" image>
-                    <v-img class="carte" v-if="movie.poster_url" :src="movie.poster_url" height="275" cover></v-img>
+                <v-card class="mb-2" raised width="187" color="grey lighten-1" image>
+                    <v-img class="carte" v-if="movie.poster_url" :src="movie.poster_url" lazy-src="" height="275" cover>
+                        <template v-slot:placeholder>
+                            <v-row class="fill-height ma-0" align="center" justify="center">
+                                <v-progress-circular indeterminate color="white"></v-progress-circular>
+                            </v-row>
+                        </template>
+                    </v-img>
                     <v-img class="carte" v-if="!movie.poster_url" src="@/assets/poster404.jpg" height="275" cover></v-img>
                 </v-card>
                 <span>{{ (movie.title || movie.original_title) }}</span>
