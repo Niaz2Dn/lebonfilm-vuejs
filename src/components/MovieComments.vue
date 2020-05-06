00a73c5763f79003c422466db4fbbd39e6ca569b
Mohamed Nizamuddin <mohamed.nizamuddin10a@gmail.com>
2020-05-06 11:34:24 +0200

    add movie

---
+++ src/components/MovieComments.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="ma-4 comments mb-8 pb-8">
+        <div class="mt-8 mb-2 display-1 font-weight-medium grey--text">
+            <span v-if="this.comments.length !== 0">Comments: ({{ this.comments.length }})</span>
+            <span v-else>Comments: </span>
+        </div>
+        <div class="comment-input ">
+            <v-text-field class="ma-4" ref="newComment" v-model="newComment" prepend-icon="mdi-comment" placeholder="Add a comment" color="light-blue" dense></v-text-field>
+            <v-btn @click="addComment" color="light-blue" class="white--text ma-4">
+                <div class="font-weight-black subtitle-1">Add a comment</div>
+            </v-btn>
+            <div class="comment" v-for="(comment, i) in comments" :key="i">
+                <v-list-item>
+                    <v-list-item-avatar v-if="!comment.image">
+                        <v-img :src="comment.image" size="50"></v-img>
+                    </v-list-item-avatar>
+                    <v-list-item-avatar v-else color="grey" size="50"></v-list-item-avatar>
+                    <v-list-item-content class="mb-4">
+                        <v-list-item-title class="font-weight-medium">{{ comment.username }}</v-list-item-title>
+                        <v-list-item-title class="font-weight-regular">{{ comment.date }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            <span class="com grey--text text--darken-1">{{ comment.content }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+// import axios from "axios";
+
+export default {
+    name: 'MovieComments',
+    props: ['movie'],
+    data: function() {
+        return {
+            backgroundImage: "",
+            trailerLoaded: false,
+            visitedMovies: [],
+            comments: [],
+            newComment: "",
+            isLiked: false,
+            nbLikes: 0,
+            username: "bob"
+        };
+    },
+}
+</script>
--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,5 +1,145 @@
 <template>
-    <div class="login">
-        <h1>This is a Movie page</h1>
+    <div class="movie">
+        <v-container :style="{ backgroundImage: 'url(' + movie.backdrop_url + ')' }" class="image pa-8" fluid pb-3>
+            <v-row align="start" justify="end" class="mr-8 pr-8">
+                <v-card class="pr-8 pt-4 mr-8 mt-8" width="600" color="transparent" outlined>
+                    <v-card-title :title="movie.original_title || movie.title" class="display-2 font-weight-black white--text">{{ (movie.original_title || movie.title) | truncate(movie.original_title || movie.title, 33, "...")}}
+                        <span :title="movie.newDate" class="ml-2 op mb-8 pd-8 body-1 font-weight-regular white--text">{{ movie.newDate }}</span>
+                    </v-card-title>
+                    <v-card-text class="op body-1 font-weight-regular white--text" :title="movie.overview">{{ movie.overview | truncate(movie.overview, 250, "...") }}</v-card-text>
+                    <v-rating class="ml-4 op1" :title="movie.newRating + '/5'" v-model="movie.newRating" color="amber" background-color="amber darken-2" empty-icon="$ratingFull" dense readonly></v-rating>
+                    <v-card-actions>
+                        <v-dialog @click:outside="resetTrailer" width="1080" overlay-opacity="0.2">
+                            <template v-slot:activator="{ on }">
+                                <v-btn fab v-on="on" class="ml-2 ma-2" @click="trailer()" small color="light-blue">
+                                    <v-icon dense medium color="white">mdi-play</v-icon>
+                                </v-btn>
+                            </template>
+                            <LazyYoutubeVideo v-if="trailerLoaded" :src="movie.trailer_url" autoplay/>
+                        </v-dialog>
+                        <v-btn v-if="isLiked" fab class="ml-2 ma-2" @click="like(movie.id)" small color="pink">
+                            <v-icon dense medium color="white">mdi-heart</v-icon>
+                        </v-btn>
+                        <v-btn v-else fab outlined class="ml-2 ma-2" @click="like(movie.id)" small color="white">
+                            <v-icon dense medium color="white">mdi-heart</v-icon>
+                        </v-btn>
+                        <span class="white--text font-weight-medium">{{ this.nbLikes }}</span>
+                    </v-card-actions>
+                </v-card>
+            </v-row>
+        </v-container>
     </div>
 </template>
+
+
+<script>
+import axios from "axios";
+import LazyYoutubeVideo from "vue-lazy-youtube-video";
+
+export default {
+    name: "Home",
+    data: function() {
+        return {
+            movie: "",
+            trailerLoaded: false,
+            comments: [],
+            newComment: "",
+            isLiked: false,
+            nbLikes: 0,
+            movieDetailsUrl: "https://lebonfilm-prod.herokuapp.com/movie/details"
+        };
+    },
+    filters: {
+        truncate: function(text, length, suffix) {
+            if (text && text.length > length) {
+                return text.substring(0, length) + suffix;
+            } else {
+                return text;
+            }
+        }
+    },
+    components: {
+        LazyYoutubeVideo
+    },
+    props: ["id"],
+    mounted() {
+        axios.get(this.movieDetailsUrl)
+        .then(res => {
+            if (res.data.result) {
+                this.movie = res.data.result;
+                this.movie["newDate"] = this.movie.release_date.substring(0, 4);
+                this.movie["newRating"] = (this.movie.vote_average * 5) / 10;
+                console.log(this.movie);
+            }
+        })
+        .catch(err => {
+            if (err.response.status == 404 || err.response.status == 500) {
+                console.log(err.response.data.error_message)
+            }
+        })
+    },
+    methods: {
+        trailer() {
+          this.trailerLoaded = true;
+        },
+    //     addComment() {
+    //   this.comments.push({
+    //     image: "https://www.randomuser.me/api/portraits/men/34.jpg",
+    //     content: this.newComment,
+    //     username: this.username
+    //   });
+    //   this.newComment = "";
+    // },
+    // like() {
+    //   if (this.isLiked) {
+    //     this.nbLikes--;
+    //     this.isLiked = false;
+    //   } else {
+    //     this.nbLikes++;
+    //     this.isLiked = true;
+    //   }
+    // },
+        resetTrailer() {
+        this.trailerLoaded = false;
+        }
+    },
+    // watch: {
+    //     nbLikes(newNbLikes) {
+    //         this.nbLikes = newNbLikes;
+    //     }
+    // }
+};
+</script>
+
+<style scoped>
+.image {
+    height: 100vh;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    box-shadow: inset 0 0 0 50vw rgb(0, 0, 0, 0.2);
+}
+.op {
+    opacity: 65%;
+}
+.op1 {
+    opacity: 95%;
+}
+.comment-input {
+    color: white;
+    border: 2px solid white;
+    border-radius: 5px;
+    background-color: white;
+    display: block;
+    text-decoration: none;
+}
+.comment {
+    color: black;
+}
+.com {
+    white-space: normal;
+    word-wrap: break-word;
+    display: block;
+    margin: 0 20px 0 20px;
+}
+</style>
