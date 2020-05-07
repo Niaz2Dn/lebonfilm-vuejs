17e3977eaac677ae613a8b057e0047e41cbd1431
Mohamed Nizamuddin <mohamed.nizamuddin10a@gmail.com>
2020-05-07 11:51:56 +0200

    tmp

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -134,25 +134,10 @@ export default {
             })
             this.details = false;
         }
-        if (this.id !== this.movie.tmdb_id) {
-            this.details = false;
-            axios.get(this.movieDetailsUrl+"?tmdb_id="+this.id)
-            .then(res => {
-                if (res.data.result) {
-                    this.movie = res.data.result;
-                    this.movie["new_date"] = this.movie.release_date.substring(0, 4);
-                    this.movie["new_rating"] = (this.movie.vote_average * 5) / 10;
-                    this.details = true;
-                }
-            })
-            .catch(err => {
-                if (err.response.status == 404 || err.response.status == 500) {
-                    console.log(err.response.data.error_message)
-                }
-            });
-            this.getLikes();
-            this.getComments();
-            this.$root.$on('username', (res) => {this.username = res});
+    },
+    computed: {
+        getFullPath() {
+            return this.$route.path
         }
     },
     methods: {
@@ -242,6 +227,26 @@ export default {
         },
         resetTrailer() {
             this.trailerLoaded = false;
+        },
+        reload() {
+            this.details = false;
+            axios.get(this.movieDetailsUrl+"?tmdb_id="+this.id)
+            .then(res => {
+                if (res.data.result) {
+                    this.movie = res.data.result;
+                    this.movie["new_date"] = this.movie.release_date.substring(0, 4);
+                    this.movie["new_rating"] = (this.movie.vote_average * 5) / 10;
+                    this.details = true;
+                }
+            })
+            .catch(err => {
+                if (err.response.status == 404 || err.response.status == 500) {
+                    console.log(err.response.data.error_message)
+                }
+            });
+            this.getLikes();
+            this.getComments();
+            this.$root.$on('username', (res) => {this.username = res});
         }
     },
     watch: {
@@ -257,8 +262,8 @@ export default {
         recoMovies(newRecoMovies) {
             this.recoMovies = newRecoMovies;
         },
-        movie(newMovie) {
-            this.movie = newMovie;
+        getFullPath() {
+            this.reload();
         }
     }
 };
